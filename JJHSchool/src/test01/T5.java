d2a5be942d27a082d1d81b80fe19fe5ab2bd1ea2
JEONGJH00 <jason523930@gmail.com>
2022-09-13 09:05:31 +0900

    김병수 교수 자바

---
+++ JJHSchool/src/test01/T5.java
@@ -0,0 +1,19 @@
+package test01;
+import java.util.Scanner;
+
+public class T5 {
+
+	public static void main(String[] args) {	// 짝수와 홀수 구별하기 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("정수를 입력하시오: ");
+		int num = sc.nextInt();
+		
+		if(num%2==0)
+			System.out.println("입력된 정수는 짝수입니다.");
+		else
+			System.out.println("입력된 정수는 홀수입니다.");
+		
+	}
+
+}
