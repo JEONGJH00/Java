a60cb4a9e249bf17e77849a1a14816aed3e21332
JEONGJH00 <jason523930@gmail.com>
2022-09-27 10:18:02 +0900

    add

--- JJH/src/exercise01/E1.java
+++ JJH/src/exercise01/E1.java
@@ -1,10 +1,24 @@
 package exercise01;
+import java.io.BufferedReader;
+import java.util.*;
 
 public class E1 {
 
 	public static void main(String[] args) {
-		System.out.println("E1");
+		Scanner sc= new Scanner(System.in);
+		int price = sc.nextInt();
+		int count = sc.nextInt();
+		int sum=0;
+		for(int i=0;i<count;i++) {
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			sum += a*b;
+		}
+		if(sum==price)
+			System.out.println("Yes");
+		else
+			System.out.println("No");
 		
 	}
-
+	
 }
--- JJHSchool/src/test01/T13.java
+++ JJHSchool/src/test01/T13.java
@@ -3,6 +3,7 @@ package test01;
 public class T13 {
 
 	public static void main(String[] args) {
+		
 		String a[]= {"Clubs","Diamonds","Hearts","Spades"};
 		String b[]={"2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"};
 		
