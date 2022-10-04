84bcc61445003f4898bfd199e5782acb2fe029e8
JEONGJH00 <jason523930@gmail.com>
2022-10-04 10:30:22 +0900

    add

---
+++ JJHSchool/src/test01/T15.java
@@ -0,0 +1,22 @@
+package test01;
+
+class Sports{
+	String getName() {	return "아직 결정되지 않음";		}
+	int getPlayers() {	return 0;	}
+}
+
+class Soccer extends Sports{
+	@Override
+	String getName() {	return "축구";	}
+	@Override
+	int getPlayers() {	return 11;	}
+}
+
+public class T15 {
+	public static void main(String[] args) {
+		Soccer s = new Soccer();
+		System.out.println("경기이름 : "+s.getName());
+		System.out.println("경기자수 : "+s.getPlayers());
+	}
+
+}
