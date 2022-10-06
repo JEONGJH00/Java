48bd2557618f4a4051e7b0dcd30b80650a73335d
JEONGJH00 <jason523930@gmail.com>
2022-10-06 21:35:23 +0900

    설탕 배달

--- JJH/src/exercise01/E2.java
+++ JJH/src/exercise01/E2.java
@@ -19,7 +19,6 @@ import java.util.StringTokenizer;
 
 public class E2 {
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
     	String s = bf.readLine();
     	StringTokenizer st = new StringTokenizer(s);
