8236c68884bd62de6d62f20ed0bd4857e23f0062
JEONGJH00 <jason523930@gmail.com>
2022-10-06 20:31:44 +0900

    달팽이는 올라가고 싶다

--- JJH/src/exercise01/E1.java
+++ JJH/src/exercise01/E1.java
@@ -1,24 +1,23 @@
 package exercise01;
+
+import java.io.IOException;
 import java.io.BufferedReader;
-import java.util.*;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-public class E1 {
-
-	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		int price = sc.nextInt();
-		int count = sc.nextInt();
-		int sum=0;
-		for(int i=0;i<count;i++) {
-			int a=sc.nextInt();
-			int b=sc.nextInt();
-			sum += a*b;
-		}
-		if(sum==price)
-			System.out.println("Yes");
-		else
-			System.out.println("No");
-		
-	}
-	
+public class E1{
+    public static void main(String[] args) throws IOException{
+    	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    	String s = bf.readLine();
+    	StringTokenizer st = new StringTokenizer(s);
+    	int A = Integer.parseInt(st.nextToken());
+    	int B = Integer.parseInt(st.nextToken());
+    	int V = Integer.parseInt(st.nextToken());
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	double dday =(double)(V-B)/(A-B);
+    	bw.write((int)Math.ceil(dday)+"\n");
+    	bw.close();
+    }
 }
