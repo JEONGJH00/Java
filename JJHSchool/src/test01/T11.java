e155f333c886debafa05d189d4035b811b91e1c3
JEONGJH00 <jason523930@gmail.com>
2022-10-11 10:42:32 +0900

    숫자 추측 게임

--- JJHSchool/src/test01/T11.java
+++ JJHSchool/src/test01/T11.java
@@ -1,26 +1,26 @@
 package test01;
+
 import java.util.Scanner;
 
-public class T11 { //숫자 추측 게임
+public class T11 { // 숫자 추측 게임
 
 	public static void main(String[] args) {
-		final int MAX =100;
 		Scanner sc = new Scanner(System.in);
-		int n=0;
-		int answer = (int)(Math.random()*100+1);
+		int n = 0;
+		int answer = (int) (Math.random() * 100 + 1);
 		int guess;
-		do 
-		{
+		do {
 			System.out.println("숫자를 입력하세요 : ");
 			guess = sc.nextInt();
 			n++;
-			if(guess<answer)
+			if (guess < answer)
 				System.out.println("제시한 숫자가 낮습니다.");
-			if(guess>answer)
+			if (guess > answer)
 				System.out.println("제시한 숫자가 높습니다.");
-		} while(guess !=answer);
-		
-		System.out.println("정답입니다. 시도한 횟수는 "+n+"회 입니다.");	
+		} while (guess != answer);
+
+		System.out.println("정답입니다. 시도한 횟수는 " + n + "회 입니다.");
+		sc.close();
 	}
 
 }//
