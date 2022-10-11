0b042f208c2cd36c5b93aa3907fd4c84d4f10458
JEONGJH00 <jason523930@gmail.com>
2022-10-11 10:44:19 +0900

    짝수와 홀수 구별하기

--- JJHSchool/src/test01/T05.java
+++ JJHSchool/src/test01/T05.java
@@ -1,19 +1,20 @@
 package test01;
+
 import java.util.Scanner;
 
 public class T05 {
 
-	public static void main(String[] args) {	// 짝수와 홀수 구별하기 
+	public static void main(String[] args) { // 짝수와 홀수 구별하기
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("정수를 입력하시오: ");
 		int num = sc.nextInt();
-		
-		if(num%2==0)
+
+		if (num % 2 == 0)
 			System.out.println("입력된 정수는 짝수입니다.");
 		else
 			System.out.println("입력된 정수는 홀수입니다.");
-		
+		sc.close();
 	}
 
 }//
