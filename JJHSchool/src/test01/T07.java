20ecacc4bdeeba5c74ee79fac3367469dbf9a69b
JEONGJH00 <jason523930@gmail.com>
2022-10-11 10:43:22 +0900

    case문을 이용하여 피자 가격을 출력

--- JJHSchool/src/test01/T07.java
+++ JJHSchool/src/test01/T07.java
@@ -2,21 +2,23 @@ package test01;
 
 import java.util.Scanner;
 
-public class T07 {	// case문을 이용하여 피자 가격을 출력
-
+public class T07 { // case문을 이용하여 피자 가격을 출력
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("피자 종류를 입력하시오 : ");
-		
+
 		String model = sc.next();
-		int price =0;
-		
-		switch(model) {
-		case "콤비네이션" : price = 20000; break;
+		int price = 0;
+
+		switch (model) {
+			case "콤비네이션":
+				price = 20000;
+				break;
 		}
-		
-		System.out.println("가격은 "+price+"원 입니다.");
+
+		System.out.println("가격은 " + price + "원 입니다.");
+		sc.close();
 	}
 
 }//
