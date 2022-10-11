befbc604691ccc124f014c13fdf8f787a1d52fee
JEONGJH00 <jason523930@gmail.com>
2022-10-11 10:43:02 +0900

    피보나치 수열

--- JJHSchool/src/test01/T12.java
+++ JJHSchool/src/test01/T12.java
@@ -8,20 +8,20 @@ public class T12 {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("출력할 항의 개수 : ");
 		int n = sc.nextInt();
-		
-		int a1=0;
-		int a2=1;
+
+		int a1 = 0;
+		int a2 = 1;
 		int a3;
-		
+
 		System.out.println(a1);
 		System.out.println(a2);
-		
-		for(int i=1;i<=n-2;i++) {
-			a3=a1+a2;
+
+		for (int i = 1; i <= n - 2; i++) {
+			a3 = a1 + a2;
 			System.out.println(a3);
-			a1=a2;
-			a2=a3;
-		}	
+			a1 = a2;
+			a2 = a3;
+		}
+		sc.close();
 	}
 }//
-
