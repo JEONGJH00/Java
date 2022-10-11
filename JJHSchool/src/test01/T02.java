1e937a76d89412ed9ef3a5ed9d00ec3a03b978fa
JEONGJH00 <jason523930@gmail.com>
2022-10-11 10:45:25 +0900

    사용자로부터 입력받은 두 수 더하기

--- JJHSchool/src/test01/T02.java
+++ JJHSchool/src/test01/T02.java
@@ -1,4 +1,5 @@
 package test01;
+
 import java.util.Scanner;
 
 public class T02 {
@@ -7,16 +8,18 @@ public class T02 {
 
 		// 사용자로부터 입력받은 두 수 더하기
 		Scanner sc = new Scanner(System.in);
-		int x,y,sum;
-		
+		int x, y, sum;
+
 		System.out.println("첫번째 숫자를 입력하세요 : ");
-		x=sc.nextInt();
-		
+		x = sc.nextInt();
+
 		System.out.println("두번째 숫자를 입력하세요 : ");
-		y=sc.nextInt();
-		
-		sum = x+y;
-		System.out.println("두 수의 합은 "+sum+" 입니다.");
+		y = sc.nextInt();
+
+		sum = x + y;
+		System.out.println("두 수의 합은 " + sum + " 입니다.");
+
+		sc.close();
 	}
 
 }
