8de3854001a3fed19c4721b0dc7196646c98a873
JEONGJH00 <jason523930@gmail.com>
2022-10-22 17:07:07 +0900

    별찍기 계단식 / 피라미드

--- JJHSchool/src/test01/T08.java
+++ JJHSchool/src/test01/T08.java
@@ -3,27 +3,25 @@ package test01;
 public class T08 {
 
 	public static void main(String[] args) {
-		
-		/* 계단식
-		for (int i = 1; i < 6; i++) 
-		{
-			for (int k = 0; k < i; k++) 
-			{
-				System.out.print("*");
-			}
-			System.out.println();
-		} */
-		
-		/* 피라미드식
-		for(int i=1;i<=5;i++) 
-		{
-			for(int k=1;k<5-i;k++) 
+
+		/*
+		 * 계단식
+		 * for (int i = 1; i < 6; i++)
+		 * {
+		 * for (int k = 0; k < i; k++)
+		 * {
+		 * System.out.print("*");
+		 * }
+		 * System.out.println();
+		 * }
+		 */
+
+		for (int i = 1; i <= 5; i++) {
+			for (int k = 1; k <= 5 - i; k++)
 				System.out.print(" ");
-			for(int j=1;j<=2*i-1;j++) 
+			for (int j = 1; j <= 2 * i - 1; j++)
 				System.out.print("*");
 			System.out.println();
-		} */
+		}
 	}
 }//
-
-
