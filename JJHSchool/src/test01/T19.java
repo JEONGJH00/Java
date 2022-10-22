01a08401d64275c99f8aea0510e3753dce221fe2
JEONGJH00 <jason523930@gmail.com>
2022-10-22 17:07:30 +0900

    클래스 상속

---
+++ JJHSchool/src/test01/T19.java
@@ -0,0 +1,45 @@
+package test01;
+
+class Point {
+    private int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    protected void move(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class ColorPoint extends Point {
+    private String color;
+
+    public ColorPoint(int x, int y, String color) {
+        super(x, y);
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
+
+public class T19 {
+    public static void main(String[] args) {
+        Point p = new Point(0, 0);
+        System.out.println(p.getX() + " " + p.getY());
+        ColorPoint cp = new ColorPoint(0, 0, "Red");
+        System.out.println(cp.getX() + " " + cp.getY() + " " + cp.getColor());
+    }
+}
